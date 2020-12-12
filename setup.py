424c1f809e6aa32f8b5f00f63e22f093e5550fb4
wyz23x2 <52805709+wyz23x2@users.noreply.github.com>
2020-12-12 21:56:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 import setuptools
 setuptools.setup(
     name = 'relationlist',
-    version = '1.2.1',
+    version = '2.0.0b1',
     author = 'wyz23x2',
     author_email = 'wyz23x2@163.com',
     description = 'A module that contains a class that supports relations.',
     long_description = '''See `the official documentation <https://relationlist.readthedocs.io>`_\ .''',
     packages = setuptools.find_packages(),
+    install_requires=['green>=3.2'],
+    python_requires='>=3.8, !=3.9.0b2',
     classifiers = [
         "License :: OSI Approved :: MIT License",
-        "Development Status :: 6 - Mature",
+        "Development Status :: 4 - Beta",
         "Operating System :: Microsoft :: Windows :: Windows 8.1",
         "Operating System :: Microsoft :: Windows :: Windows 10",
-        "Operating System :: Microsoft :: Windows :: Windows 8",  # NOTE: Windows 8 support will be dropped in v2.0.
+        "Operating System :: Microsoft :: Windows :: Windows 8", # NOTE: 8 support will be dropped in v2.3.
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.8",
